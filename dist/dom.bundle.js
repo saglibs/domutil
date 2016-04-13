70e8e5ef192b4cc7c528371cc57bd3623ad9044e
sagnitude <leyicong@gmail.com>
2016-04-13 18:36:46 +0800

    now selector wrapper support document and window.

--- dist/dom.bundle.js
+++ dist/dom.bundle.js
@@ -6633,18 +6633,27 @@ var Mini = require('coreutil/mini');
 /**
  * Search elements in the current ResultSet.
  *
- * @param {Array|Element|NodeList} ele ResultSet to check
- * @param {String} selector CSS selector string
+ * @param {Array|Element|NodeList|Node} ele ResultSet to check
+ * @param {String|NodeList|Node|Window} selector CSS selector string
  * @returns {*} ResultSet of elements
  */
 function findElement(ele, selector) {
 
-    //if is RS, wrap it
-    if (typeof selector !== 'string'
-        && (selector instanceof NodeList
-        || selector instanceof Element
-        || Mini.isArrayLike(selector))) {
-        return wrap(selector);
+    //if selector is RS or window/document, wrap it
+    if (typeof selector !== 'string') {
+        if (selector instanceof NodeList
+            || selector instanceof Element
+            || Mini.isArrayLike(selector)) {
+            return wrap(selector);
+        }
+        if (selector instanceof Node) {
+            return wrap(selector);
+        }
+        if (selector === window) {
+            //css operations not allowed for window, but event operations allowed.
+            //TODO: add dom event module, make a splitter and change code here
+            return wrap(document);
+        }
     }
 
     if (ele === document) {
