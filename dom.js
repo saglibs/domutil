aa6fa037c07d9f75696b5397615db79d37c52d37
sagnitude <leyicong@gmail.com>
2016-05-11 00:13:48 +0800

    moved project files as maven project.

--- dom.js
+++ dom.js
@@ -1,8 +1,8 @@
-var DOM = require('./src/cssselector');
+var DOM = require('./src/main/javascript/cssselector');
 
 var Core = require('coreutil/core');
-var RS = require('./src/domresultset');
-var Attr = require('./src/cssattribute');
+var RS = require('./src/main/javascript/domresultset');
+var Attr = require('./src/main/javascript/cssattribute');
 
 Core.extend(Core, RS);
 Core.extend(Core, Attr);
