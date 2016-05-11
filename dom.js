c4eba04dc948f19dabfea90addedea257cd81d16
sagnitude <leyicong@gmail.com>
2016-05-11 14:38:59 +0800

    now able to build from maven source instead of npm dependencies

--- dom.js
+++ dom.js
@@ -1,8 +1,9 @@
-var DOM = require('./src/main/javascript/cssselector');
+var DOM = require('./src/cssselector');
 
+// var Core = require('./target/dependencies/core');
 var Core = require('coreutil/core');
-var RS = require('./src/main/javascript/domresultset');
-var Attr = require('./src/main/javascript/cssattribute');
+var RS = require('./src/domresultset');
+var Attr = require('./src/cssattribute');
 
 Core.extend(Core, RS);
 Core.extend(Core, Attr);
--- gulpfile.js
+++ gulpfile.js
@@ -2,5 +2,5 @@ var gulp = require('gulp');
 var gulpTask = require('coreutil/gulptask');
 
 gulp.task('default', function () {
-    return gulpTask(['./dom.js'], [], "./src/main/resources/dist");
+    return gulpTask(['./dom.js'], [], "./dist");
 });
